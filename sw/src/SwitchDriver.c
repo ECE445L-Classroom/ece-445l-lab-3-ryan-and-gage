3d879c0e1ec72d0904d978edb48b58347ca15501
RyanFol <ryanfolmar246@utexas.edu>
2024-09-18 18:48:27 -0500

    Commented Starting Points

--- sw/src/SwitchDriver.c
+++ sw/src/SwitchDriver.c
@@ -1,1 +1,15 @@
 #include <stdint.h>
+
+
+
+
+
+void switchinit()
+{
+	//Place here all the initalization for the different intended switches
+}
+
+uint8_t switchRead(uint8_t inputswitch)
+{
+	//Place here the debounced output of the selected switch
+}
--- sw/src/SwitchDriver.h
+++ sw/src/SwitchDriver.h
@@ -1,9 +1,14 @@
 //SwitchDriver.h
 
+#define leftswitch 0
+#define rightswitch 1
+#define enterswitch 2
 
+#include <stdint.h>
 
-
-
+//Initalizes the switches
 void switchinit(void);
 
-void switchinit(void);
+//Reads the switch
+uint8_t switchRead(uint8_t inputswitch);
+
