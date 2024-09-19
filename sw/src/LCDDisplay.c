392ae1584e86ab504d7533cd1fd9e5373f6f83cf
millergage <gage.miller@utexas.edu>
2024-09-19 09:48:59 -0500

    Added Function Definitions

--- sw/src/LCDDisplay.c
+++ sw/src/LCDDisplay.c
@@ -1,10 +1,15 @@
 
-const uint16_t lightModeClock [] =
+const uint16_t lightModeClock [] = ;
 	
-const uint16_t darkModeClock [] =
-	
+const uint16_t darkModeClock [] = ;
 
 
+struct hourHand 
+{
+	uint16_t color = 0;
+	uint
+};
+	
 
 void LCDDisplay_Init()
 {
@@ -13,4 +18,29 @@ void LCDDisplay_Init()
   SSD1306_OutBuffer();
 }
 
-void Draw
+void DisplayTimeLightMode(uint16_t hour, uint16_t minute, uint16_t second);
+{
+	ST7735_DrawBitmap(4, 159, lightModeClock, 120, 160);
+	ST7735_DrawLine
+	
+}
+
+void DisplayTimeDarkMode()
+{
+	ST7735_DrawBitmap(4, 159, darkModeClock, 120, 160);
+	
+}
+
+void DisplayDigitalTime()
+{
+	
+	
+}
+
+void AlarmStateBlinking() //takes current display state and flashes it 
+{
+	//wait for a timer that triggers a couple times a second
+	
+	
+	
+}
