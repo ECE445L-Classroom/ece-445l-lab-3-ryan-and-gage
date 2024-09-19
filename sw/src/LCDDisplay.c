12374c82b832121a979de10081c2478d95e081af
millergage <gage.miller@utexas.edu>
2024-09-19 10:09:57 -0500

    Basic Light Mode Dark Mode Functions

--- sw/src/LCDDisplay.c
+++ sw/src/LCDDisplay.c
@@ -4,12 +4,18 @@ const uint16_t lightModeClock [] = ;
 const uint16_t darkModeClock [] = ;
 
 
-struct hourHand 
+struct clockHand 
 {
-	uint16_t color = 0;
-	uint
-};
+	uint16_t color;
+	uint16_t length;
+	//uint16_t startPosition; 
+	uint16_t time;
+
+} hourHand, minuteHand, secondHand;
 	
+uint8_t = currentSecondDisplayed;
+uint8_t = currentMinuteDisplayed;
+uint8_t = currentHourDisplayed;
 
 void LCDDisplay_Init()
 {
@@ -21,14 +27,18 @@ void LCDDisplay_Init()
 void DisplayTimeLightMode(uint16_t hour, uint16_t minute, uint16_t second);
 {
 	ST7735_DrawBitmap(4, 159, lightModeClock, 120, 160);
-	ST7735_DrawLine
+	ST7735_Line(0,0,0,0,hourHand.color);
+	ST7735_Line(0,0,0,0,hourHand.color);
+	ST7735_Line(0,0,0,0,hourHand.color);
 	
 }
 
 void DisplayTimeDarkMode()
 {
 	ST7735_DrawBitmap(4, 159, darkModeClock, 120, 160);
-	
+	ST7735_Line(0,0,0,0,hourHand.color);
+	ST7735_Line(0,0,0,0,hourHand.color);
+	ST7735_Line(0,0,0,0,hourHand.color);
 }
 
 void DisplayDigitalTime()
@@ -40,7 +50,7 @@ void DisplayDigitalTime()
 void AlarmStateBlinking() //takes current display state and flashes it 
 {
 	//wait for a timer that triggers a couple times a second
-	
+	while()
 	
 	
 }
