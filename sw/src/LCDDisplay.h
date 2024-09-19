e8b95492e5117eefb1e985ac9bd8613d19a69f3f
millergage <gage.miller@utexas.edu>
2024-09-19 12:22:14 -0500

    bruh

---
+++ sw/src/LCDDisplay.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+void LCDDisplay_Init();
+void DisplayTimeLightMode(uint16_t hour, uint16_t minute, uint16_t second);
+void DisplayTimeDarkMode();
+void DisplayTimeDarkMode();
+void DisplayDigitalTime();
+void AlarmStateBlinking();
